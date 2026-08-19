99d19f088c8ebc89923820e13489d8ef5b6c999d
jieunlee99 <lll3346@naver.com>
2026-08-19 18:44:20 +0900

    chore: Spring Boot 프로젝트 초기 환경 구성

---
+++ src/main/java/com/knit_proj/KnitProjApplication.java
@@ -0,0 +1,13 @@
+package com.knit_proj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnitProjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KnitProjApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/knit_proj/KnitProjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.knit_proj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KnitProjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
